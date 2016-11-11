70c22dd364dbbaec7e9921f2866da832a1186f2f
85758586 <85758586@github.com>
2016-11-11 14:51:34 +0800

    语法分析加入错误处理

---
+++ HardASM/src/main/java/com/hard/hardasm/HSEBuilder.java
@@ -0,0 +1,8 @@
+package main.java.com.hard.hardasm;
+
+/**
+ * <h3></h3>
+ * Created by root on 2016/11/11.
+ */
+public class HSEBuilder {
+}
--- HardASM/src/main/java/com/hard/hardasm/SyntacticAnalyzer.java
+++ HardASM/src/main/java/com/hard/hardasm/SyntacticAnalyzer.java
@@ -16,13 +16,24 @@ public class SyntacticAnalyzer {
         for (int i = 0; i < tokens.size(); i++){
             Token currentToken = tokens.get(i);
             String currentValue = currentToken.getValue() == null ? "" : currentToken.getValue().toUpperCase();
+            int nextTokenType = getNextTokenType(tokens, i + 1);
 
             switch (currentToken.getTokenType()){
                 case Token.TOKEN_TYPE_IDENT:
                     switch (currentValue){
                         case Instruction.ADD:
-                            int code = Instruction.getInstructionCode(Instruction.ADD);
-                            Log.d(TAG, "instruction code is " + code);
+                            if (nextTokenType == Token.TOKEN_TYPE_INT){
+                                int next2TokenType = getNextTokenType(tokens, i + 2);
+                                if (next2TokenType == Token.TOKEN_TYPE_INT){
+                                    int code = Instruction.getInstructionCode(Instruction.ADD);
+                                    //i = skipParsedTokens(i, 2);
+                                    Log.d(TAG, "instruction code is " + code);
+                                }else {
+                                    throw new IllegalTokenException("error token " + next2TokenType);
+                                }
+                            }else {
+                                throw new IllegalTokenException("error token " + nextTokenType);
+                            }
                             break;
                         default:
                             break;
@@ -43,5 +54,17 @@ public class SyntacticAnalyzer {
         }
     }
 
+    private static int getNextTokenType(List<Token> tokens, int index){
+        if (index > tokens.size() - 1){
+            return Token.END_OF_TOKEN_STREAM;
+        }
+
+        return tokens.get(index).getTokenType();
+    }
+
+    private static int skipParsedTokens(int startIndex, int skipCount){
+        startIndex += skipCount;
+        return startIndex;
+    }
 
 }
