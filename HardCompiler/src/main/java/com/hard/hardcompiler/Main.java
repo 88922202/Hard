52354fda012e4a3524bb442077288f780a0e1242
85758586 <85758586@github.com>
2016-11-15 15:45:05 +0800

    建立HardCompiler工程

---
+++ HardCompiler/src/main/java/com/hard/hardcompiler/Main.java
@@ -0,0 +1,11 @@
+package com.hard.hardcompiler;
+
+/**
+ * <h3></h3>
+ * Created by root on 2016/11/15.
+ */
+public class Main {
+    public static void main(String args[]){
+        System.out.print("HardCompiler started.");
+    }
+}
